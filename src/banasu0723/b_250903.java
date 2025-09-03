1c8317df096a5d7374b8bbf47749cef8dde9ea4e
banasu0723 <banasu0723@gmail.com>
2025-09-03 23:28:54 +0900

    250903(수) _ 수 찾기

---
+++ src/banasu0723/b_250903.java
@@ -0,0 +1,42 @@
+package banasu0723;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class b_250903 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < N; i++){
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 이진 탐색을 위한 정렬
+        Arrays.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            int num = Integer.parseInt(st.nextToken());
+
+            if(Arrays.binarySearch(A, num) >= 0){
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
+        }
+
+        System.out.println(sb);
+
+
+    }
+}
