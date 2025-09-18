edbc5cfa7d443c8ff30969ed4cda536823b4703f
나의찬 <cab07036@naver.com>
2025-09-18 09:46:17 +0900

    250918 문제 11650

--- src/Uechann/sorting/u_250918.java
+++ src/Uechann/sorting/u_250918.java
@@ -6,7 +6,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 /* 
@@ -54,8 +53,8 @@ public class u_250918 {
 
         dots.stream()
                 .sorted(
-                    Comparator.comparingInt((int[] d) -> d[0])
-                              .thenComparing(d -> d[1])
+                    Comparator.comparingInt((int[] d) -> d[0])  //1차 비교 기준
+                              .thenComparing(d -> d[1])         //2차 비교 기준
                 )
                 .forEach(d -> System.out.println(d[0] + " " + d[1]));
     }
