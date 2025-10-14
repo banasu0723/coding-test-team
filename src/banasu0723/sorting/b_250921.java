c68681730b70ce792812ba9dfecd615d9153ea8e
banasu0723 <banasu0723@gmail.com>
2025-10-14 18:22:39 +0900

    251014(화) _ 1697

---
+++ src/banasu0723/sorting/b_250921.java
@@ -0,0 +1,11 @@
+package banasu0723.sorting;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class b_250921 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+}
